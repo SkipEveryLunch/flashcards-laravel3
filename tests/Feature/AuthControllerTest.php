b7867e68507f4ff26af8147b59454253e034a8c6
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-08-22 17:05:19 +0900

    in AuthControllerTest, put common variables into private

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -17,7 +17,10 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-
+    private $first_name = "John";
+    private $last_name = "Smith";
+    private $email = "js@test.io";
+    private $password = 1234;
     public function test_no_user_before_register()
     {
         $users = User::get();
@@ -25,49 +28,37 @@ class ExampleTest extends TestCase
     }
     public function test_register_returns_code_201()
     {
-        $first_name = "John";
-        $last_name = "Smith";
-        $email = "js@test.io";
-        $password = 1234;
         $res = $this->post('/api/register',[
-            "first_name" => $first_name,
-            "last_name" => $last_name,
-            "email"=>$email,
-            "password"=>$password
+            "first_name" => $this->first_name,
+            "last_name" => $this->last_name,
+            "email"=>$this->email,
+            "password"=>$this->password
         ]);
         $res->assertStatus(201);
     }
     public function test_register_increases_number_of_user()
     {
-        $first_name = "John";
-        $last_name = "Smith";
-        $email = "js@test.io";
-        $password = 1234;
         $numberBefore = User::get()->count(); 
         $res = $this->post('/api/register',[
-            "first_name" => $first_name,
-            "last_name" => $last_name,
-            "email"=>$email,
-            "password"=>$password
+            "first_name" => $this->first_name,
+            "last_name" => $this->last_name,
+            "email"=>$this->email,
+            "password"=>$this->password
         ]);
         $numberAfter = User::get()->count();
         $this->assertEquals(1,$numberAfter-$numberBefore);
     }
     public function test_login_works_with_proper_arguments()
     {
-        $first_name = "John";
-        $last_name = "Smith";
-        $email = "js@test.io";
-        $password = 1234;
         $this->post('/api/register',[
-            "first_name" => $first_name,
-            "last_name" => $last_name,
-            "email"=>$email,
-            "password"=>$password
+            "first_name" => $this->first_name,
+            "last_name" => $this->last_name,
+            "email"=>$this->email,
+            "password"=>$this->password
         ]);
         $res = $this->post('/api/login',[
-            "email"=>$email,
-            "password"=>$password
+            "email"=>$this->email,
+            "password"=>$this->password
         ]);
         $res->assertStatus(200);
     }
@@ -76,7 +67,6 @@ class ExampleTest extends TestCase
         $user = User::factory()->create();
         Sanctum::actingAs($user);
         $res = $this->get('/api/user');
-        dump($res);
         $res->assertStatus(200);
     }
 }
